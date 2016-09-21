c9d7a9d7e013c8266efb13e2da64702ab530b8a4
ganesh <ganeshmaridi@gmail.com>
2016-09-22 00:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var requestone={
+    title:'request-one||ganesh',
+    heading:'request-one',
+    date:'sept5,2016',
+    content:
+            `<p>
+                need some content hereneed some content hereneed some content hereneed some content herevneed some content herevvvvneed some content hereneed some content hereneed some content herev
+                need some content hereneed some content herevvneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here
+                need some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here.
+            </p>
+            <p>
+                need some content hereneed some content hereneed some content hereneed some content herevneed some content herevvvvneed some content hereneed some content hereneed some content herev
+                need some content hereneed some content herevvneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here
+                need some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here.
+            </p>
+            <p>
+                need some content hereneed some content hereneed some content hereneed some content herevneed some content herevvvvneed some content hereneed some content hereneed some content herev
+                need some content hereneed some content herevvneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here
+                need some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content hereneed some content here.
+            </p>`
+};            
+
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.date;
+    var htmlTemplate=`
+        <html>
+                <head>
+                <title>    
+                    ${title}
+                </title>
+                <meta name='viewport' content='width=device-width,initial-scale=1'/>
+                <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <div class='container'> 
+                    <div>
+                    <a href='/'>HOME</a>    
+                    </div> 
+                    <div>
+                    ${heading}
+                    </div>
+                    <div>
+                    ${date}    
+                    </div>
+                    <div>
+                    ${content}
+                    </div>
+                </body>
+        </html>
+    `; 
+    return htmlTemplate;
+}    
+
+    
+
+
+
+
+
 app.get('/request-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'request-one.html'));
+     res.send(createTemplate(requestone));
 });
 app.get('/request-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'request-two.html'));
