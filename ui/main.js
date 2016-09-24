12bf3619021763eb7d6e998070259631a9955584
ganesh <ganeshmaridi@gmail.com>
2016-09-24 19:18:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,5 +19,13 @@ var nameInput=document.getElementById('name');
 nameInput=name.value;
 var submit=document.getElementById('sub-btn');
 submit.onclick=function() {
-    
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+        
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
 };
