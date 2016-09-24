3acdf580a3e6d6df5e6754afd1e15f28768409f0
ganesh <ganeshmaridi@gmail.com>
2016-09-24 18:07:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
-var button
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function() {
+        if(request.readystate===XMLHttpRequest.DONE){
+            if(request.status===200) {
+                var counter=request.responseText;
+                var span=document.getElemetById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://ganeshmaridi.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
