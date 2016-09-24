e8ddcf76914309f6e2e174a4edc076589393907a
ganesh <ganeshmaridi@gmail.com>
2016-09-24 17:50:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,1 @@
-console.log('Loaded!');
-var element=document.getElementById("text");
-element.innerHTML="newvalue";
-var img=document.getElementById("ganni");
-img.onclick=function(){
-  img.style.marginLeft='100px';  
-};
+var button
